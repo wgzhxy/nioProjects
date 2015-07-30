764d49967db2364f15338f4c7975baafe5268e5d
东往 <guangzhong.wgz@alibaba-inc.com>
2015-07-30 13:06:39 +0800

    annotation提交

---
+++ src/org/annotation/FruitRunMain.java
@@ -0,0 +1,14 @@
+package org.annotation;
+/***********Êä³ö½á¹û***************/
+public class FruitRunMain {
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        
+        FruitInfoUtil.getFruitInfo(Apple.class);
+        
+    }
+
+}
---
+++ src/org/annotation/TestMain.java
@@ -0,0 +1,19 @@
+package org.annotation;
+
+import java.lang.annotation.Inherited;
+import java.lang.annotation.Documented;
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+/**
+ * TODO Comment of TestMain
+ * 
+ * @author guangzhong.wgz
+ */
+public class TestMain {
+
+    public static void main(String[] args) {
+
+    }
+
+}
